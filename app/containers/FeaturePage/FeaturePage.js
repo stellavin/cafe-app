c81e471e22309b6b5532c99177c95bca6e7efd39
stella <stellavinmaris@gmail.com>
2019-06-14 17:42:14 +0300

    added dropdown and file upload

--- app/containers/FeaturePage/FeaturePage.js
+++ app/containers/FeaturePage/FeaturePage.js
@@ -6,12 +6,15 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import './style.scss';
+import Select from 'react-select';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+const techCompanies = [
+  { label: "Side", value: "Side" },
+  { label: "Main Course", value: "Main Course" }
+];
 export default class FeaturePage extends React.Component {
-  // eslint-disable-line react/prefer-stateless-function
-
-  // Since state and props are static,
-  // there's no need to re-render this component
+  
   shouldComponentUpdate() {
     return false;
   }
@@ -28,27 +31,38 @@ export default class FeaturePage extends React.Component {
         </Helmet>
         <h2>Add Menu Item</h2>
 
-        <section>
-          <div className="input-box">
-            <text>Type</text>
-            <input></input>
+          <div className="row margin">
+            <div className="col-md-2">Type</div>
+            <div className="col-md-5">
+              <Select options={ techCompanies } />
+            </div>
           </div>
 
-          <div className="input-box">
-            <text>Name</text>
-            <input></input>
+          <div className="row margin">
+            <div className="col-md-2">Name</div>
+            <div className="col-md-5">
+               <input></input>
+            </div>
           </div>
 
-          <div className="input-box">
-            <text>Price</text>
-            <input></input>
+          <div className="row margin">
+            <div className="col-md-2">Price</div>
+            <div className="col-md-5">
+               <input></input>
+            </div>
           </div>
-          <div className="input-box">
-            <text>Photo</text>
-            <input></input>
+
+          <div className="row margin">
+            <div className="col-md-2">Photo</div>
+            <div className="col-md-3">
+            <input type="file" className="inputfile"></input>
+            </div>
           </div>
+       
+
+       
           <button>Save Item</button>
-        </section>
+       
       
       </div>
     );
